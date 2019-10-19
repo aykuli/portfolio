c1122d31be279c1188d0a32c02cf1bfc20b613d9
Aykuli <aykuli@ya.ru>
2019-10-20 00:47:49 +0700

    feat: add swiper with mouse

--- scripts/slider.js
+++ scripts/slider.js
@@ -46,3 +46,60 @@ document.querySelector('.arrow__next').addEventListener('click', function() {
         nextItem(currentItem);
     }
 });
+
+// swiper by Shalyapin
+const swipeDetect = (el) => {
+    let surface = el;
+    let startX =  0;
+    let startY = 0;
+    let distX = 0;
+    let distY = 0;
+    
+    let startTime = 0;
+    let elapsedTime = 0;
+
+    let threshold = 250;
+    let restraint = 100;
+    let allowedTime = 300;
+
+    surface.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+        startX = e.pageX;
+        startY = e.pageY;
+        startTime = new Date().getTime();
+    });
+
+    surface.addEventListener('mouseup', function(e) {
+        e.preventDefault();
+        distX = e.pageX - startX;
+        distY = e.pageY - startY;
+        elapsedTime = new Date().getTime() - startTime;
+
+        console.log('distX = ', distX);
+        console.log('distY = ', distY);
+        console.log('elapsedTime = ', elapsedTime);
+
+        if (elapsedTime <= allowedTime) {
+            console.log('(elapsedTime <= allowedTime)');
+            if ( ( Math.abs(distX) > threshold ) && ( Math.abs(distY) <= restraint ) ) {
+                console.log('Math.abs(distX > threshold) && Math.abs(distY <= restraint)');
+                if (distX > 0) {
+                    console.log('--------------   distX > 0');
+                    if (isEnabled) {
+                        previousItem(currentItem);
+                    }
+                } else {
+                    console.log('--------------   distX < 0');
+                    if (isEnabled) {
+                        nextItem(currentItem);
+                    }
+                }
+            }
+        }
+    });
+}
+
+let el = document.querySelector('.slider');
+
+swipeDetect(el);
+// END of swiper by Shalyapin
